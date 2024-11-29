2a2aee74b97111c2f03a3d8b5374d09dae4e04eb
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-11-29 13:11:37 -0300

    add: estrutura base para teste da função calcular_area_circunferencia

---
+++ U3/src/area_circunferencia.c
@@ -0,0 +1,6 @@
+#include "area_circunferencia.h"
+#include <math.h>
+
+double calcular_area_circunferencia(double raio) {
+	return raio;
+}
---
+++ U3/src/area_circunferencia.h
@@ -0,0 +1,6 @@
+#ifndef AREA_CIRCUNFERENCIA_H
+#define AREA_CIRCUNFERENCIA_H
+
+double calcular_area_circunferencia(double raio);
+
+#endif
