b19025b7aba9c933fc33fb9f3c2fa15126b5e66f
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-11-29 13:19:04 -0300

    add: testes para calcular_area_circunferencia

---
+++ U3/tests/test_area_circunferencia.c
@@ -0,0 +1,25 @@
+#include "unity.h"
+#include "area_circunferencia.h"
+
+void setUp() {}
+void tearDown() {}
+
+void test_area_valida() {
+	TEST_ASSERT_DOUBLE_WITHIN(0.01, 12.57, calcular_area_circunferencia(2.0));
+}
+
+void test_raio_zero() {
+	TEST_ASSERT_DOUBLE_WITHIN(0.0, calcular_area_circunferencia(0.0));
+}
+
+void test_raio_negativo() {
+	TEST_ASSERT_DOUBLE_WITHIN(-1.0, calcular_area_circunferencia(-3.0));
+}
+
+int main() {
+	UNITY_BEGIN();
+	RUN_TEST(test_area_valida);
+	RUN_TEST(test_raio_zero);
+	RUN_TEST(test_raio_negativo);
+	return UNITY_END();
+}
