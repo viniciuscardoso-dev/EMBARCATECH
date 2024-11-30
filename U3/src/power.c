c5077df33d9d8a8aca3e8f41c6db99bb40bebedb
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-11-30 18:55:39 -0300

    fix: erro de digitação

--- U3/src/power.c
+++ U3/src/power.c
@@ -3,9 +3,9 @@
 int main() {
 	int a, b, pow = 1;
 
-	printf("Digite o primeiro número: "):
+	printf("Digite o primeiro número: ");
 	scanf("%d", &a);
-	printf("Digite o segundo número: "):
+	printf("Digite o segundo número: ");
 	scanf("%d", &p);
 
 	for (int i = 1; i <= b; i++) {
