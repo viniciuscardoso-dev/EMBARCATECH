2338f77b28c7564e030e8b56692c94aaae85424d
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-11-30 18:18:07 -0300

    add: exemplo U3C3 depuração

---
+++ U3/src/power.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+int main() {
+	int a, b, pow = 1;
+
+	printf("Digite o primeiro número: "):
+	scanf("%d", &a);
+	printf("Digite o segundo número: "):
+	scanf("%d", &p);
+
+	for (int i = 1; i <= b; i++) {
+		pow = pow * a;
+	}
+
+	printf("%d", a);
+}
