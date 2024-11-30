377531198be6482c68613fac596efbd579fcbd00
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-11-30 14:30:24 -0300

    feat: função inverter_numero compátivel com a tarefa proposta

--- U3/src/inverter_numero.c
+++ U3/src/inverter_numero.c
@@ -1,5 +1,24 @@
-#include "inverter_numero.h"
+#include <stdio.h>
 
-int inverter_numero(int numero) {
-	return numero;
+int inverterNumero(int numero) {
+	int invertido = 0, resto;
+		while (numero != 0) {
+			resto = numero % 10;
+			invertido = invertido * 10 + resto;
+			numero /= 10;
+		}
+
+	return invertido;
+}
+
+int main() {
+	int numero, numeroInvertido;
+	printf("Digite um número de três dígitos: ");
+	scanf("%d", &numero);
+	if (numero >= 100 && numero <= 999) {
+		numeroInvertido = inverterNumero(numero);
+		printf("O número invertido é: %d\n", numeroInvertido);
+	} else {
+		printf("Número inválido. Digite um número entre 100 e 999.\n");
+	}
 }
