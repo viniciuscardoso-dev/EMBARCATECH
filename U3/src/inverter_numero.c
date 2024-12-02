62a091bb8eee961ab3ab396935b6c64a0071860d
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-12-02 17:13:19 -0300

    refactor: função inverter número

--- U3/src/inverter_numero.c
+++ U3/src/inverter_numero.c
@@ -1,24 +1,13 @@
-#include <stdio.h>
+#include "inverter_numero.h"
 
-int inverterNumero(int numero) {
-	int invertido = 0, resto;
-		while (numero != 0) {
-			resto = numero % 10;
-			invertido = invertido * 10 + resto;
-			numero /= 10;
-		}
-
-	return invertido;
-}
-
-int main() {
-	int numero, numeroInvertido;
-	printf("Digite um número de três dígitos: ");
-	scanf("%d", &numero);
-	if (numero >= 100 && numero <= 999) {
-		numeroInvertido = inverterNumero(numero);
-		printf("O número invertido é: %d\n", numeroInvertido);
-	} else {
-		printf("Número inválido. Digite um número entre 100 e 999.\n");
+int inverter_numero(int numero) {
+	if (numero < 100 || numero > 999) {
+		return -1;
 	}
+
+	int centena = numero / 100;
+	int dezena = (numero / 10) % 10;
+	int unidade = numero % 10;
+
+	return unidade * 100 + dezena * 10 + centena;
 }
