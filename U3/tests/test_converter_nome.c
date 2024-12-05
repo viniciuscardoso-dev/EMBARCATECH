b6396f3e8120225ba8d9d91c76b0a248438029d1
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-12-05 16:27:19 -0300

    test: converter nome(binário/hexadecimal)

---
+++ U3/tests/test_converter_nome.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdlib.h>
+#include "../src/converter_nome.h"
+#include "unity/unity.h"
+
+void setUp() {}
+void tearDown() {}
+
+void test_nome_para_hexadecimal() {
+	char* resultado = nome_para_hexadecimal("abc");
+	TEST_ASSERT_NOT_NULL(resultado);
+	TEST_ASSERT_EQUAL_STRING("61 62 63", resultado);
+	free(resultado);
+
+	resultado = nome_para_hexadecimal("Test");
+	TEST_ASSERT_NOT_NULL(resultado);
+	TEST_ASSERT_EQUAL_STRING("54 65 73 74", resultado);
+	free(resultado);
+}
+
+void test_nome_para_binario() {
+	char* resultado = nome_para_binario("abc");
+	TEST_ASSERT_NOT_NULL(resultado);
+	TEST_ASSERT_EQUAL_STRING("01100001 01100010 01100011", resultado);
+	free(resultado);
+
+	resultado = nome_para_binario("Test");
+	TEST_ASSERT_NOT_NULL(resultado);
+	TEST_ASSERT_EQUAL_STRING("01010100 01100101 01110011 01110100", resultado);
+	free(resultado);
+}
+
+int main() {
+	UNITY_BEGIN();
+	RUN_TEST(test_nome_para_hexadecimal);
+	RUN_TEST(test_nome_para_binario);
+	return UNITY_END();
+}
