587bea0bbb5019c0bfabbfaf91947affaf9ddfbf
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2024-12-05 21:01:22 -0300

    feat: converter_nome_binario

--- U3/src/converter_nome.c
+++ U3/src/converter_nome.c
@@ -1,8 +1,18 @@
 #include "converter_nome.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 char* nome_para_hexadecimal(const char* nome){
-	(void)nome;
-	return "ok";
+	size_t len = strlen(nome);
+	char* resultado = (char*)malloc(len * 3);
+	if (!resultado) return NULL;
+
+	for (size_t i = 0; i < len; i++){
+		sprintf(resultado + i * 3, "%02X ", (unsigned char)nome[i]);
+	}
+	resultado[len * 3 - 1] = '\0';
+	return resultado;
 }
 
 char* nome_para_binario(const char* nome){
